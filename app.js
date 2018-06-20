830d536dc2989eb8d69dcb6a5bdbcafa587e0f76
Mai Tấn Đạt <maitandat1507@gmail.com>
2018-06-20 16:46:05 +0700

    planning project

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 // BUDGET CONTROLLER
+var budgetController = (function() {
+	//
+})();
+
+
+// UI CONTROLLER
+var UIController = (function () {
+	//
+})();
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+	
+	document.querySelector('.add__btn').addEventListener('click', function() {
+		
+		// 1. Get input data
+
+		// 2. Add item to budget controller
+
+		// 3. Add item to UI
+
+		// 4. Calculate budget
+
+		// 5. Display the budget on UI
+
+	});
+
+})(budgetController, UIController);
