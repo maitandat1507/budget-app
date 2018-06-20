6062ace6d2bd25ab1a71a42840584b95e70dd3db
mrtada <maitandat1507@gmail.com>
2018-06-21 00:37:19 +0700

    create init function

--- app.js
+++ app.js
@@ -7,13 +7,24 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function () {
 
+	var DOMstrings = {
+		inputType: '.add__type',
+		inputDescription: '.add__description',
+		inputValue: '.add__value',
+		inputBtn: '.add__btn',
+	};
+
 	return {
-		getinput: function() {
+		getInput: function() {
 			return {
-				type: document.querySelector('.add__type').value,
-				description: document.querySelector('.add__description').value,
-				value: document.querySelector('.add__value').value,
+				type: document.querySelector(DOMstrings.inputType).value,
+				description: document.querySelector(DOMstrings.inputDescription).value,
+				value: document.querySelector(DOMstrings.inputValue).value,
 			}
+		},
+
+		getDOMstrings: function() {
+			return DOMstrings;
 		}
 	}
 
@@ -23,10 +34,24 @@ var UIController = (function () {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+	var setupEventListeners = function() {
+		var DOM = UICtrl.getDOMstrings();
+
+		document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+		document.addEventListener('keypress', function(event) {
+
+			if (event.keyCode === 13 || event.which === 13) {
+				ctrlAddItem();
+			}
+
+		});
+	};
+
 	var ctrlAddItem = function() {
 
 		// 1. Get input data
-		var input = UICtrl.getinput();
+		var input = UICtrl.getInput();
 		console.log(input);
 
 		// 2. Add item to budget controller
@@ -38,15 +63,17 @@ var controller = (function(budgetCtrl, UICtrl) {
 		// 5. Display the budget on UI
 
 	}
-	
-	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
-	document.addEventListener('keypress', function(event) {
-
-		if (event.keyCode === 13 || event.which === 13) {
-			ctrlAddItem();
+	return {
+		init: function() {
+			console.log('Application has started');
+			setupEventListeners();
 		}
-
-	});
+	}
 
 })(budgetController, UIController);
+
+controller.init();
+
+
+
