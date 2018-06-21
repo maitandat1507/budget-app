4f5979399c240b809f07ae285ad6563ffaa7097c
Mai Tấn Đạt <maitandat1507@gmail.com>
2018-06-21 16:14:24 +0700

    display budget

--- app.js
+++ app.js
@@ -105,6 +105,10 @@ var UIController = (function () {
 		inputBtn: '.add__btn',
         incomeContainer: '.income__list',
         expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
 	};
 
 	return {
@@ -153,6 +157,15 @@ var UIController = (function () {
             });
 
             fieldsArr[0].focus();
+        },
+
+        displayBudget: function(obj) {
+
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+
         },
 
 		getDOMstrings: function() {
